b082570e774448027982b9d5f1b9f3a1bbe7081b
Hariharan Tamilarasan <hariharant@gislen.com>
2022-06-20 23:56:15 +0530

    Subject Types

--- Angular-Rxjs/src/app/app.component.ts
+++ Angular-Rxjs/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AsyncSubject, BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Rxjs';
+
+  constructor() {
+    let subject = new Subject<number>();
+
+  }
 }
